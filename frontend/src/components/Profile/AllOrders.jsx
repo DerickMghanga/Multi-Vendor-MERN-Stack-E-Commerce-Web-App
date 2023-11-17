df24b64040f1e42db5f187b0ba55429c8ae2f27a
DerickMghanga <derickmghanga3329@gmail.com>
2023-11-17 03:38:01 +0300

    Orders section

--- frontend/src/components/Profile/AllOrders.jsx
+++ frontend/src/components/Profile/AllOrders.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { AiOutlineArrowRight } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import { DataGrid } from "@mat"
 
 const AllOrders = () => {
     const orders = [
@@ -23,6 +26,68 @@ const AllOrders = () => {
             orderStatus: "Processing",
         },
     ];
+
+    const columns = [
+        { field: "id", headerName: "Order ID", minWidth: 150, flex: 0.7 },
+    
+        {
+          field: "status",
+          headerName: "Status",
+          minWidth: 130,
+          flex: 0.7,
+          cellClassName: (params) => {
+            return params.getValue(params.id, "status") === "Delivered"
+              ? "greenColor"
+              : "redColor";
+          },
+        },
+        {
+          field: "itemsQty",
+          headerName: "Items Qty",
+          type: "number",
+          minWidth: 130,
+          flex: 0.7,
+        },
+    
+        {
+          field: "total",
+          headerName: "Total",
+          type: "number",
+          minWidth: 130,
+          flex: 0.8,
+        },
+    
+        {
+          field: " ",
+          flex: 1,
+          minWidth: 150,
+          headerName: "",
+          type: "number",
+          sortable: false,
+          renderCell: (params) => {
+            return (
+              <>
+                <Link to={`/user/order/${params.id}`}>
+                  <Button>
+                    <AiOutlineArrowRight size={20} />
+                  </Button>
+                </Link>
+              </>
+            );
+          },
+        },
+    ];
+
+    const row = [];
+    orders && orders.forEach((item)=> {
+        row.push({
+            id: item._id,
+            itemsQty: item.orderItems.length,
+            total: "US$ " + item.totalPrice,
+            status: item.orderStatus,
+        })
+    })
+
 
   return (
     <div className='pl-8 pt-1'>
